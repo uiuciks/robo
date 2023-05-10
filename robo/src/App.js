6b3550daf3788d0fa5e243ddbf0d53315203898a
suci <suciwidhi22@gmail.com>
2023-05-10 18:41:14 +0800

    robo

---
+++ robo/src/App.js
@@ -0,0 +1,33 @@
+import CardList from './components/cardlist/cardlist.component';
+import { robots } from './robot';
+import SearchBox from './components/searchbox/searchbox.component';
+import { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchField: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value })
+
+  }
+  render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    })
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ robo/src/components/cardlist/cardlist.component.jsx
@@ -0,0 +1,20 @@
+import Card from "../card/card.component";
+
+const CardList = ({ robots }) => {
+    return (
+        <div>
+            {
+                robots.map((user, i) => {
+                    return (
+                        <Card key={i}
+                            id={robots[i].id}
+                            name={robots[i].name}
+                            email={robots[i].email} />
+                    )
+                })}
+        </div>
+    )
+
+}
+
+export default CardList;
